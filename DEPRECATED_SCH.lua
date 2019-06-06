0930886db6fd51e9b468947b73182234b403049a
Tunaliz <irtehlili@gmail.com>
2019-06-06 15:36:57 +0800

    Fixed bug where MB set was always equipped on casting

--- DEPRECATED_SCH.lua
+++ DEPRECATED_SCH.lua
@@ -593,7 +593,7 @@ function midcast(spell)
     elseif spell.skill == 'Enfeebling Magic' and spell.type == 'WhiteMagic' then -- to do: better rule for this.
         equip(sets.midcast.MndEnfeebling)
     elseif spell.type == 'BlackMagic' then
-        if mBurst then
+        if mBurst.value == true then
             equip(sets.midcast.MB[nukeModes.current])
         else
             equip(sets.midcast.nuking[nukeModes.current])
